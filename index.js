42df4b680adf3ad4ed11ab930bc76726f126a797
street21 <98771723+street21@users.noreply.github.com>
2023-01-17 21:35:16 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+
+    document.querySelectorAll('button')[0].addEventListener('click', function(){
+        var b=document.querySelector('img').getAttribute('alt');
+        itsTime(b,1);
+    
+    })
+    
+    document.querySelectorAll('button')[0].addEventListener('keydown', function(event){
+        itsTime(event.key,2);
+    })
+
+
+
+
+function itsTime(key, num){
+    switch (key) {
+        case 'r':
+            var ricked=new Audio('sound/Never_Gonna_Give_You_Up_Original.mp3');
+            ricked.play();
+            $('h1').text('You got rick rolled!')
+            $('p').text('Get good!')
+            $('p')[1].remove();
+            $('.top').css('padding','70px')
+            $('.top').css('background-color','#112D4E')
+            $('.middle').css('background-color','#DBE2EF')
+            $('.bottom').css('background-color','#3F72AF')
+            break;
+    
+        default:
+            if(num==2){
+                alert("not really pressing 'r' are ya!");
+            }
+            
+            break;
+    }
+}
